4fde88f1395a47c97e07c4d6c42ee9156d7ff5fb
Alejandro Villalba <villalbaa2017@gmail.com>
2026-05-25 18:39:26 -0300

    mejora tienda pro

--- app/zapatillas/page.tsx
+++ app/zapatillas/page.tsx
@@ -43,6 +43,242 @@ export default function Zapatillas() {
         </div>
 
       </div>
+      
+      <div className="grid md:grid-cols-3 gap-8">
+
+        <div className="bg-zinc-900 rounded-3xl overflow-hidden">
+
+          <img
+            src="/zapa1.jpg"
+            alt="Nike"
+            className="w-full h-72 object-cover"
+          />
+
+          <div className="p-6">
+
+            <h2 className="text-2xl font-bold mb-2">
+              Nike Dunk Low
+            </h2>
+
+            <p className="text-zinc-400 mb-4">
+              Importadas 🔥
+            </p>
+
+            <p className="text-3xl font-bold mb-5">
+              $180.000
+            </p>
+
+            <a
+              href="https://wa.me/5491100000000"
+              target="_blank"
+              className="bg-white text-black px-5 py-3 rounded-xl inline-block"
+            >
+              Comprar
+            </a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      
+      <div className="grid md:grid-cols-3 gap-8">
+
+        <div className="bg-zinc-900 rounded-3xl overflow-hidden">
+
+          <img
+            src="/zapa1.jpg"
+            alt="Nike"
+            className="w-full h-72 object-cover"
+          />
+
+          <div className="p-6">
+
+            <h2 className="text-2xl font-bold mb-2">
+              Nike Dunk Low
+            </h2>
+
+            <p className="text-zinc-400 mb-4">
+              Importadas 🔥
+            </p>
+
+            <p className="text-3xl font-bold mb-5">
+              $180.000
+            </p>
+
+            <a
+              href="https://wa.me/5491100000000"
+              target="_blank"
+              className="bg-white text-black px-5 py-3 rounded-xl inline-block"
+            >
+              Comprar
+            </a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+      
+      <div className="grid md:grid-cols-3 gap-8">
+
+        <div className="bg-zinc-900 rounded-3xl overflow-hidden">
+
+          <img
+            src="/zapa1.jpg"
+            alt="Nike"
+            className="w-full h-72 object-cover"
+          />
+
+          <div className="p-6">
+
+            <h2 className="text-2xl font-bold mb-2">
+              Nike Dunk Low
+            </h2>
+
+            <p className="text-zinc-400 mb-4">
+              Importadas 🔥
+            </p>
+
+            <p className="text-3xl font-bold mb-5">
+              $180.000
+            </p>
+
+            <a
+              href="https://wa.me/5491100000000"
+              target="_blank"
+              className="bg-white text-black px-5 py-3 rounded-xl inline-block"
+            >
+              Comprar
+            </a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+      
+      <div className="grid md:grid-cols-3 gap-8">
+
+        <div className="bg-zinc-900 rounded-3xl overflow-hidden">
+
+          <img
+            src="/zapa1.jpg"
+            alt="Nike"
+            className="w-full h-72 object-cover"
+          />
+
+          <div className="p-6">
+
+            <h2 className="text-2xl font-bold mb-2">
+              Nike Dunk Low
+            </h2>
+
+            <p className="text-zinc-400 mb-4">
+              Importadas 🔥
+            </p>
+
+            <p className="text-3xl font-bold mb-5">
+              $180.000
+            </p>
+
+            <a
+              href="https://wa.me/5491100000000"
+              target="_blank"
+              className="bg-white text-black px-5 py-3 rounded-xl inline-block"
+            >
+              Comprar
+            </a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      
+      <div className="grid md:grid-cols-3 gap-8">
+
+        <div className="bg-zinc-900 rounded-3xl overflow-hidden">
+
+          <img
+            src="/zapa1.jpg"
+            alt="Nike"
+            className="w-full h-72 object-cover"
+          />
+
+          <div className="p-6">
+
+            <h2 className="text-2xl font-bold mb-2">
+              Nike Dunk Low
+            </h2>
+
+            <p className="text-zinc-400 mb-4">
+              Importadas 🔥
+            </p>
+
+            <p className="text-3xl font-bold mb-5">
+              $180.000
+            </p>
+
+            <a
+              href="https://wa.me/5491100000000"
+              target="_blank"
+              className="bg-white text-black px-5 py-3 rounded-xl inline-block"
+            >
+              Comprar
+            </a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+      
+      <div className="grid md:grid-cols-3 gap-8">
+
+        <div className="bg-zinc-900 rounded-3xl overflow-hidden">
+
+          <img
+            src="/zapa1.jpg"
+            alt="Nike"
+            className="w-full h-72 object-cover"
+          />
+
+          <div className="p-6">
+
+            <h2 className="text-2xl font-bold mb-2">
+              Nike Dunk Low
+            </h2>
+
+            <p className="text-zinc-400 mb-4">
+              Importadas 🔥
+            </p>
+
+            <p className="text-3xl font-bold mb-5">
+              $180.000
+            </p>
+
+            <a
+              href="https://wa.me/5491100000000"
+              target="_blank"
+              className="bg-white text-black px-5 py-3 rounded-xl inline-block"
+            >
+              Comprar
+            </a>
+
+          </div>
+
+        </div>
+
+      </div>
 
     </main>
   )
