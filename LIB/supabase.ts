9f081c4bfa5647284003530a5b5233cd3b36c524
Alejandro Villalba <villalbaa2017@gmail.com>
2026-06-03 20:18:56 -0300

    mejora tienda pro

--- LIB/supabase.ts
+++ LIB/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "TU_PROJECT_URL",
-  "TU_ANON_PUBLIC_KEY"
+  "https://vmsghazriuoaoivyimcj.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtc2doYXpyaXVvYW9pdnlpbWNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MTMzNzEsImV4cCI6MjA5NjA4OTM3MX0.mljQu1IX1fOxsGY2UKQR5G07JxD0Bhe8le6FfHHU6jYnpm install @supabase/supabase-js"
 );
